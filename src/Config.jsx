b407fd967389fe8f7b7e3c0ff1197ff1ef0f9739
sarmaakondi <avnmsarma@gmail.com>
2024-11-17 15:36:52 +1100

    Move the layout styles to Config component

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,7 +1,11 @@
 import CharacterLength from "./CharacterLength";
 
 const Config = () => {
-    return <CharacterLength />;
+    return (
+        <div className="w-[34rem] flex items-center justify-between bg-componentBg px-8 py-5">
+            <CharacterLength />
+        </div>
+    );
 };
 
 export default Config;
